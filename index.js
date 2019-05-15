4bbe180679e08a41c4737cc9c829d2b82002092a
rxl3 <rxlee3@gmail.com>
2019-05-15 10:24:44 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const soap = require('soap');
+const csvParse = require('csv-parse');
+const fs = require('fs');
+const url = 'https://soacs-uat.afgonline.com.au/assess/migration/MigrateApplicationPS?wsdl';
+const args = {_xml: '<mig:MigrateApplicationRequest xmlns:mig="http://xmlns.oracle.com/assess/MigrateApplicationPS"><mig:lrn>?</mig:lrn></mig:MigrateApplicationRequest>'};
+const stream = fs.createWriteStream('logs.txt', {flags: 'a'});
+const errorStream = fs.createWriteStream('errors.txt', {flags: 'a'});
+
+let appsToMigrate = [];
+const parser = csvParse({
+    delimiter: ','
+}, (err, output) => {
+    output.shift(); // Remove title row
+    appsToMigrate = output;
+    console.log(appsToMigrate);
+    appsToMigrate.forEach(app => {
+        migrateApp(app[0], app[1], app[2]);
+        console.log('Migrating Application: ' + app[0] + ' with options: ' + app[1] + ', ' + app[2]);
+    });
+});
+fs.createReadStream('apps.csv').pipe(parser);
+
+function migrateApp(appID, optionA, optionB) {
+    stream.write('(' + appID + ') Attempting to migrate app\n');
+    soap.createClientAsync('./MigrateApplicationPS.xml')
+    .then(client => {
+        args['_xml'] = '<mig:MigrateApplicationRequest xmlns:mig="http://xmlns.oracle.com/assess/MigrateApplicationPS"><mig:lrn>' + appID + '</mig:lrn></mig:MigrateApplicationRequest>';
+        return client.MigrateAsync(args);
+    })
+    .then(result => {
+        stream.write('(' + appID + ') Successful migration\n');
+    })
+    .catch(err => {
+        // console.log(err.response.body);
+        if (!err.response.body.includes('<successfullyLodged>true</successfullyLodged>')) {
+            stream.write('(' + appID + ') FAILED migrating\n');
+            stream.write('Error log: ' + err + '\n');
+            errorStream.write(appID + ': ' + err + '\n');
+        } else {
+            stream.write('(' + appID + ') Successful migration\n');
+        }
+    });
+}
