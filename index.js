7eac5bd0941986a910a7a26dfb00eb2f5565a475
Ryan Lee <ryan.lee@afgonline.com.au>
2019-05-16 15:33:10 +0800

    Fix error logging

--- index.js
+++ index.js
@@ -12,31 +12,41 @@ const parser = csvParse({
 }, (err, output) => {
     output.shift(); // Remove title row
     appsToMigrate = output;
-    // console.log(appsToMigrate);
+    console.log(appsToMigrate.length + ' Apps Found.');
     appsToMigrate.forEach(app => {
-        migrateApp(app[0]);//, app[1], app[2]);
-        console.log('Migrating Application: ' + app[0]);// + ' with options: ' + app[1] + ', ' + app[2]);
+        migrateApp(app[0])
     });
 });
 fs.createReadStream(process.argv[2]).pipe(parser);
 
 function migrateApp(appID, optionA, optionB) {
-    stream.write('(' + appID + ') Attempting to migrate app\n');
-    soap.createClientAsync('./MigrateApplicationPS.xml')
-    .then(client => {
-        args['_xml'] = '<mig:MigrateApplicationRequest xmlns:mig="http://xmlns.oracle.com/assess/MigrateApplicationPS"><mig:lrn>' + appID + '</mig:lrn></mig:MigrateApplicationRequest>';
-        return client.MigrateAsync(args);
-    })
-    .then(result => {
-        stream.write('(' + appID + ') Successful migration\n');
-    })
-    .catch(err => {
-        if (!err.response.body.includes('<successfullyLodged>true</successfullyLodged>')) {
-            stream.write('(' + appID + ') FAILED migrating\n');
-            stream.write('Error log: ' + err + '\n');
-            errorStream.write(appID + ': ' + err + '\n');
-        } else {
-            stream.write('(' + appID + ') Successful migration\n');
-        }
-    });
+        console.log('Attempting to migrate app: ' + appID);// + ' with options: ' + app[1] + ', ' + app[2]);
+        stream.write('(' + appID + ') Attempting to migrate app\n');
+        soap.createClientAsync('./MigrateApplicationPS.xml')
+        .then(client => {
+            args['_xml'] = '<mig:MigrateApplicationRequest xmlns:mig="http://xmlns.oracle.com/assess/MigrateApplicationPS"><mig:lrn>' + appID + '</mig:lrn></mig:MigrateApplicationRequest>';
+            return client.MigrateAsync(args);
+        })
+        .then(result => {
+            if (!result[1].includes('<successfullyLodged>true</successfullyLodged>')) {
+                stream.write('(' + appID + ') FAILED migrating\n');
+                stream.write('Error log: ' + result[1] + '\n');
+                errorStream.write(appID + ': ' + result[1] + '\n');
+                console.error('(' + appID + ') FAILED migrating');
+            } else {
+                stream.write('(' + appID + ') Successful migration\n');
+                console.log('(' + appID + ') Successful migration');
+            }
+        })
+        .catch(err => {
+            if (!err.response.body.includes('<successfullyLodged>true</successfullyLodged>')) {
+                stream.write('(' + appID + ') FAILED migrating\n');
+                stream.write('Error log: ' + err + '\n');
+                errorStream.write(appID + ': ' + err + '\n');
+                console.error('(' + appID + ') FAILED migrating');
+            } else {
+                stream.write('(' + appID + ') Successful migration\n');
+                console.log('(' + appID + ') Successful migration');
+            }
+        });
 }
